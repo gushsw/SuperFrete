e75f098069306461c18f5426b6d6b862df8089c9
Gustavo Henrique Silva <47002007+gushsw@users.noreply.github.com>
2024-08-18 16:38:30 -0300

    Update DagETLSuperFrete.py

--- airflow/dag/DagETLSuperFrete.py
+++ airflow/dag/DagETLSuperFrete.py
@@ -1,164 +1,151 @@
 import boto3 as b3
 import pendulum as pdl
 from airflow.decorators import task
-from airflow.models.baseoperator import chain
 from airflow.models.dag import DAG
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
-from airflow.providers.amazon.aws.hooks.glue_databrew import GlueDataBrewHook
 from airflow.providers.amazon.aws.sensors.glue_crawler import GlueCrawlerSensor
-from airflow.providers.amazon.aws.operators.s3 import (
-    S3CreateBucketOperator,
-    S3CreateObjectOperator,
-    S3DeleteBucketOperator,
-)
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
 from airflow.utils.trigger_rule import TriggerRule
-from tests.system.providers.amazon.aws.utils import ENV_ID_KEY, SystemTestContextBuilder, prune_logs
 
 # Configurações da DAG
-DAG_ID = 'Exemplo_glue_SF'  # SF = SuperFrete
+DAG_ID = 'ETL_glue_SF_Bronze_Silver_Gold'
 ROLE_ARN_KEY = "ROLE_ARN"  # Deve ser configurado com o ARN da role IAM correta
 
-# Dados e Script de Exemplo
+# Dados de Exemplo
 EX_DATA = """Frete1, 120,20km,0\nFrete2, 135,35km,1\nFrete3, 140,40km,0\n"""
 
-EX_SCRIPT = """
+EX_SCRIPT_BRONZE = """
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-import boto3 as b3
 
 glueContext = GlueContext(SparkContext.getOrCreate())
-
-s3_client = b3.client('s3')
-bucket_name = 'superfrete-datalake-dados'
-scripts_folder = 'scripts/'
-
 datasource = glueContext.create_dynamic_frame.from_catalog(
     database='{db_name}', table_name='input')
 
-print(f'Existem {datasource.count()} itens na tabela')
+datasource.toDF().write.format('parquet').mode("append").save('{output_path}')
+"""
 
-output_path = f's3://{bucket_name}/output'
-datasource.toDF().write.format('parquet').mode("append").save(output_path)
+EX_SCRIPT_SILVER = """
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+datasource = glueContext.create_dynamic_frame.from_catalog(
+    database='{db_name}', table_name='bronze')
+
+# Aplicar algumas transformações nos dados para a camada Silver
+transformed_data = datasource.toDF().filter(datasource["Frete"] > 130)
+
+transformed_data.write.format('parquet').mode("append").save('{output_path}')
+"""
+
+EX_SCRIPT_GOLD = """
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+datasource = glueContext.create_dynamic_frame.from_catalog(
+    database='{db_name}', table_name='silver')
+
+# Aplicar mais transformações para a camada Gold
+final_data = datasource.toDF().groupBy("Frete").agg({"Distancia": "max"})
+
+final_data.write.format('parquet').mode("append").save('{output_path}')
 """
 
 @task
 def get_role_name(arn: str) -> str:
     return arn.split("/")[-1]
 
-@task(trigger_rule=TriggerRule.ALL_DONE)
-def glue_cleanup(crawler_name: str, job_name: str, db_name: str) -> None:
-    client = b3.client("glue")
-    client.delete_crawler(Name=crawler_name)
-    client.delete_job(JobName=job_name)
-    client.delete_database(Name=db_name)
-
 # Definindo a DAG
 with DAG(
     dag_id=DAG_ID,
     schedule_interval="@once",
     start_date=pdl.datetime(2024, 8, 19),
-    tags=["Exemplo_SuperFrete"],
+    tags=["ETL_SuperFrete"],
     catchup=False,
 ) as dag:
     
-    test_context = SystemTestContextBuilder().add_variable(ROLE_ARN_KEY).build()
-
-    env_id = test_context[ENV_ID_KEY]
-    role_arn = test_context[ROLE_ARN_KEY]
-    glue_crawler_name = f"{env_id}_crawler"
-    glue_db_name = f"{env_id}_glue_db"
-    glue_job_name = f"{env_id}_glue_job"
-    bucket_name = f"{env_id}-bucket-dados"
+    role_arn = "arn:aws:iam::123456789012:role/MyGlueRole"  # Substitua pelo seu ARN real
     role_name = get_role_name(role_arn)
 
-    glue_crawler_config = {
-        "Name": glue_crawler_name,
-        "Role": role_arn,
-        "DatabaseName": glue_db_name,
-        "Targets": {"S3Targets": [{"Path": f"s3://{bucket_name}/input"}]}
-    }
+    glue_crawler_name_bronze = "crawler_bronze"
+    glue_db_name_bronze = "db_bronze"
+    glue_job_name_bronze = "job_bronze"
+    bucket_name_bronze = "superfrete-datalake-bronze"
 
-    # Tarefas do Airflow
-    create_bucket = S3CreateBucketOperator(
-        task_id="create_bucket",
-        bucket_name=bucket_name,
-    )
+    glue_crawler_name_silver = "crawler_silver"
+    glue_db_name_silver = "db_silver"
+    glue_job_name_silver = "job_silver"
+    bucket_name_silver = "superfrete-datalake-silver"
 
-    upload_parquet = S3CreateObjectOperator(
-        task_id="upload_parquet",
-        s3_bucket=bucket_name,
+    glue_crawler_name_gold = "crawler_gold"
+    glue_db_name_gold = "db_gold"
+    glue_job_name_gold = "job_gold"
+    bucket_name_gold = "superfrete-datalake-gold"
+
+    # Upload do arquivo e script para a camada Bronze
+    upload_bronze_data = S3CreateObjectOperator(
+        task_id="upload_bronze_data",
+        s3_bucket=bucket_name_bronze,
         s3_key="input/input.parquet",
         data=EX_DATA,
-        replace=True
-    )
-
-    upload_script = S3CreateObjectOperator(
-        task_id="upload_script",
-        s3_bucket=bucket_name,
-        s3_key="etl_script.py",
-        data=EX_SCRIPT.format(db_name=glue_db_name, bucket_name=bucket_name),
         replace=True,
     )
 
-    crawl_s3 = GlueCrawlerSensor(
-        task_id="crawl_s3",
-        crawler_name=glue_crawler_name,
-        config=glue_crawler_config,
+    upload_bronze_script = S3CreateObjectOperator(
+        task_id="upload_bronze_script",
+        s3_bucket=bucket_name_bronze,
+        s3_key="bronze_script.py",
+        data=EX_SCRIPT_BRONZE.format(db_name=glue_db_name_bronze, output_path=f"s3://{bucket_name_bronze}/output"),
+        replace=True,
     )
 
-    wait_for_crawl = GlueCrawlerSensor(
-        task_id="wait_for_crawl",
-        crawler_name=glue_crawler_name,
-    )
-
-    submit_glue_job = GlueJobOperator(
-        task_id="submit_glue_job",
-        job_name=glue_job_name,
-        script_location=f"s3://{bucket_name}/etl_script.py",
-        s3_bucket=bucket_name,
+    # Glue tasks para Bronze, Silver e Gold
+    submit_bronze_glue_job = GlueJobOperator(
+        task_id="submit_bronze_glue_job",
+        job_name=glue_job_name_bronze,
+        script_location=f"s3://{bucket_name_bronze}/bronze_script.py",
+        s3_bucket=bucket_name_bronze,
         iam_role_name=role_name,
         create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
     )
 
-    wait_for_job = GlueCrawlerSensor(
-        task_id="wait_for_job",
-        crawler_name=glue_crawler_name,
+    crawl_silver = GlueCrawlerSensor(
+        task_id="crawl_silver",
+        crawler_name=glue_crawler_name_silver,
     )
 
-    delete_bucket = S3DeleteBucketOperator(
-        task_id="delete_bucket",
-        trigger_rule=TriggerRule.ALL_DONE,
-        bucket_name=bucket_name,
-        force_delete=True,
+    submit_silver_glue_job = GlueJobOperator(
+        task_id="submit_silver_glue_job",
+        job_name=glue_job_name_silver,
+        script_location=f"s3://{bucket_name_silver}/silver_script.py",
+        s3_bucket=bucket_name_silver,
+        iam_role_name=role_name,
+        create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
     )
 
-    log_cleanup = prune_logs(
-        [
-            ("/aws-glue/crawlers", glue_crawler_name),
-            ("/aws/glue/jobs/logs-v2", submit_glue_job.output),
-            ("/aws/glue/jobs/error", submit_glue_job.output),
-            ("/aws/glue/jobs/output", submit_glue_job.output),
-        ]
+    crawl_gold = GlueCrawlerSensor(
+        task_id="crawl_gold",
+        crawler_name=glue_crawler_name_gold,
+    )
+
+    submit_gold_glue_job = GlueJobOperator(
+        task_id="submit_gold_glue_job",
+        job_name=glue_job_name_gold,
+        script_location=f"s3://{bucket_name_gold}/gold_script.py",
+        s3_bucket=bucket_name_gold,
+        iam_role_name=role_name,
+        create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
     )
 
     # Encadeamento das tarefas
-    chain(
-        # Configuração inicial
-        test_context,
-        create_bucket,
-        upload_parquet,
-        upload_script,
-        # Processamento de dados
-        crawl_s3,
-        wait_for_crawl,
-        submit_glue_job,
-        wait_for_job,
-        # Limpeza do ambiente
-        glue_cleanup(glue_crawler_name, glue_job_name, glue_db_name),
-        delete_bucket,
-        log_cleanup,
+    (
+        upload_bronze_data
+        >> upload_bronze_script
+        >> submit_bronze_glue_job
+        >> crawl_silver
+        >> submit_silver_glue_job
+        >> crawl_gold
+        >> submit_gold_glue_job
     )
-
-    from tests.system.utils.watcher import watcher
-
-    list(dag.tasks) >> watcher()
